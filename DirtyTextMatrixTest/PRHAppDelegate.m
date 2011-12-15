d0f6fec7d4e6365994a52853283aebcb7d75a829
Peter Hosey <prh@Four-Sided-Cheese-Grater.local>
2011-12-14 22:31:57 -0800

    Initial Commit

---
+++ DirtyTextMatrixTest/PRHAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PRHAppDelegate.m
+//  DirtyTextMatrixTest
+//
+//  Created by Peter Hosey on 2011-12-14.
+//  Copyright (c) 2011 Captured Magic, LLC. All rights reserved.
+//
+
+#import "PRHAppDelegate.h"
+
+@implementation PRHAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ DirtyTextMatrixTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DirtyTextMatrixTest
+//
+//  Created by Peter Hosey on 2011-12-14.
+//  Copyright (c) 2011 Captured Magic, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
